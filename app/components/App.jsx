213ac0ce722789f362d15ffb73bfbc049357870e
Dylan <dylanninin@gmail.com>
2016-06-09 15:33:15 +0800

    Push state into App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import Notes from './Notes';
 import uuid from 'uuid';
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Do laundry'
-    }
-];
+export default class App extends React.Component {
 
-export default () => (
-  <div>
-      <button onClick={() => console.log('add note')}> + </button>
-      <Notes notes={notes} />
-  </div>
-);
+    constructor(props){
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
+    }
+
+    render() {
+        const {notes} = this.state;
+
+        return (
+            <div>
+                <button onClick={() => console.log('add note')}> + </button>
+                <Notes notes={notes} />
+            </div>
+        )
+    }
+}
+
