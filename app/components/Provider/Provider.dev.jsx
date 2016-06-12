3554f1ab8671ecfef838e79b100315047a6502cf
Dylan <dylanninin@gmail.com>
2016-06-12 22:07:33 +0800

    Initial Provider

---
+++ app/components/Provider/Provider.dev.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AltContainer from 'alt-container';
+import chromeDebug from 'alt-utils/lib/chromeDebug';
+import alt from '../../libs/alt';
+import setup from './setup';
+
+setup(alt);
+
+chromeDebug(alt);
+
+React.Pref = require('react-addons-pref');
+
+export default ({children}) =>
+    <AltContainer flux={alt}>
+        {children}
+    </AltContainer>
